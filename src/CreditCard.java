850991cd02ba6a5ab515c4bb39f8f2ee5d1e9580
Astghik-hub <minastghik@gmail.com>
2024-12-04 14:51:09 -0500

    fourth commit

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,3 +1,6 @@
+/**
+ * This class defines a credit card using the Money and Person class
+ */
 public class CreditCard {
     // The balance of the person
     private Money balance;
@@ -18,18 +21,40 @@ public class CreditCard {
         balance = new Money(0);
     }
 
+    /**
+     * get method for balance
+     *
+     * @return the balance
+     */
     public Money getBalance() {
         return new Money(balance);
     }
 
+    /**
+     * get method for credit limit
+     *
+     * @return the credit limit
+     */
     public Money getCreditLimit() {
         return new Money(creditLimit);
     }
 
+    /**
+     * get method for the owners personal information
+     *
+     * @return the personal information
+     */
     public String getPersonals() {
         return owner.toString();
     }
 
+    /**
+     * method that charges to the CreditCard by adding the amount
+     * passed in the parameter to the balance, but only if
+     * it will not exceed the creditLimit.
+     *
+     * @param amount a certain amount of money
+     */
     public void charge(Money amount) {
         Money tempBalance = new Money(balance);
         tempBalance = tempBalance.add(amount);
@@ -40,6 +65,12 @@ public class CreditCard {
         }
     }
 
+    /**
+     * method that will make a payment on the CreditCard by subtracting
+     * the amount passed in the parameter from the balance.
+     *
+     * @param amount a certain amount of money
+     */
     public void payment(Money amount) {
         balance = balance.subtract(amount);
     }
